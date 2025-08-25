ed85dde41bb73d1960433754c097553bd2534dbc
azkanafis04 <nafisazkaalfarisi2004@mail.ugm.ac.id>
2025-08-25 02:46:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pgpbl ionic',
+  webDir: 'www'
+};
+
+export default config;
